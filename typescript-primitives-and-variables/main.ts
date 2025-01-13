0acf32a7cfda8bfd067c20e4e314006c6022fc23
Han-Zhang-Code <zyh514312@gmail.com>
2025-01-13 00:00:32 +0000

    done

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,16 @@
+const fullName: string = 'Yuhan Zhang';
+const isCool: boolean = true;
+const totalPets: number = 2;
+console.log('value of fullName: ', fullName);
+console.log('value of isCool: ', isCool);
+console.log('value of totalPets: ', totalPets);
+console.log('type of fullName: ', typeof fullName);
+console.log('type of isCool: ', typeof isCool);
+console.log('type of totalPets: ', typeof totalPets);
+const empty: null = null;
+let nothing: undefined;
+
+console.log('value of empty: ', empty);
+console.log('type of empty: ', typeof empty);
+console.log('value of nothing: ', nothing);
+console.log('type of nothing: ', typeof nothing);
