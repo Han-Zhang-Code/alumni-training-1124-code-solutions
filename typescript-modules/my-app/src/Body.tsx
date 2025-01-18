16947da4e0466963cb8dd6b968e5c9a93fa1133e
Han-Zhang-Code <zyh514312@gmail.com>
2025-01-18 18:35:16 +0000

    done

---
+++ typescript-modules/my-app/src/Body.tsx
@@ -0,0 +1,14 @@
+import BottomTextSection from './BottomTextSection';
+import MidTextSection from './MidTextSection';
+import TitleSection from './TitleSection';
+
+function Body() {
+  return (
+    <>
+      <TitleSection />
+      <MidTextSection />
+      <BottomTextSection />
+    </>
+  );
+}
+export default Body;
---
+++ typescript-modules/my-app/src/Footer.tsx
@@ -0,0 +1,4 @@
+function Footer() {
+  return <h4>Modules are fun!! Copyright LearningFuze 2024</h4>;
+}
+export default Footer;
